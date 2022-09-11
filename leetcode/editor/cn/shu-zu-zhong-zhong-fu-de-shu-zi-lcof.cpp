246ac07a3102c4bf6d6fd2290848f8c43afdacf3
liuyuming <Lym!XJ0301>
2022-09-11 22:38:01 +0800

    c++ leetcode init

---
+++ leetcode/editor/cn/shu-zu-zhong-zhong-fu-de-shu-zi-lcof.cpp
@@ -0,0 +1,40 @@
+//找出数组中重复的数字。 
+//
+// 在一个长度为 n 的数组 nums 里的所有数字都在 0～n-1 的范围内。数组中某些数字是重复的，但不知道有几个数字重复了，也不知道每个数字重复了几次。
+//请找出数组中任意一个重复的数字。 
+//
+// 示例 1： 
+//
+// 输入：
+//[2, 3, 1, 0, 2, 5, 3]
+//输出：2 或 3 
+// 
+//
+// 
+//
+// 限制： 
+//
+// 2 <= n <= 100000 
+//
+// Related Topics 数组 哈希表 排序 👍 945 👎 0
+
+using namespace std;
+#include "iostream"
+#include "vector"
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+public:
+    int findRepeatNumber(vector<int>& nums) {
+
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
+
+int main()
+{
+    Solution s;
+    vector<int> data{7, 1, 5, 3, 6, 4};
+    //vector<int> ans = s.twoSum(data,11);
+    //cout << ans[0]<<ans[1]<<endl;
+    cout<<"Hello LeetCode"<<endl;
+}
---
+++ leetcode/editor/cn/ti-huan-kong-ge-lcof.cpp
@@ -0,0 +1,38 @@
+//请实现一个函数，把字符串 s 中的每个空格替换成"%20"。 
+//
+// 
+//
+// 示例 1： 
+//
+// 输入：s = "We are happy."
+//输出："We%20are%20happy." 
+//
+// 
+//
+// 限制： 
+//
+// 0 <= s 的长度 <= 10000 
+//
+// Related Topics 字符串 👍 345 👎 0
+
+using namespace std;
+#include "iostream"
+#include "vector"
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+public:
+    string replaceSpace(string s) {
+        cout<<"sdffdf"<<endl;
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
+
+int main()
+{
+    Solution s;
+    s.replaceSpace("test");
+    vector<int> data{7, 1, 5, 3, 6, 4};
+    //vector<int> ans = s.twoSum(data,11);
+    //cout << ans[0]<<ans[1]<<endl;
+    cout<<"Hello LeetCode"<<endl;
+}
